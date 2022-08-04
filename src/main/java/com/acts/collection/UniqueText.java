5335c4093be336fa8bf4c2b44533cfd51da8caf6
past18 <pastefinivw@gmail.com>
2022-08-04 21:49:14 +0300

    Unique Text. HashSet.

---
+++ src/main/java/com/acts/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package com.acts.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String t : text) {
+            if (!check.contains(t)) {
+                rsl=false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
