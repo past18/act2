a15c10f6d224fc43c18cdbcb874fe216e7fbe594
past18 <pastefinivw@gmail.com>
2022-08-08 15:56:58 +0300

    Bank transfers.

---
+++ src/main/java/com/acts/bank/BankService.java
@@ -0,0 +1,59 @@
+package com.acts.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.put(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            if (!getAccounts(user).contains(account)) {
+                getAccounts(user).add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User u : users.keySet()) {
+            if (u.getPassport().equals(passport)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : getAccounts(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String destRequisite, double amount) {
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
